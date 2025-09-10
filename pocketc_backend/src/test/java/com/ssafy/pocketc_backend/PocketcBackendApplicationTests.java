4d53a1c97ad85b5ea5b2034b708be3fde2e86c53
DESKTOP-HA97GMI\SSAFY <sungmin91529@gmail.com>
2025-09-10 21:46:55 +0900

    clean broken submodule

---
+++ pocketc_backend/src/test/java/com/ssafy/pocketc_backend/PocketcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.pocketc_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
