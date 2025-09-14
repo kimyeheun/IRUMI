3b5b5d86d28ae6ae947197aa2a1d0e23092fbb8c
sungmin915 <sungmin91529@gmail.com>
2025-09-15 01:13:27 +0900

    feature: 로컬 실행용 h2 추가

--- pocketc_backend/src/main/java/com/ssafy/pocketc_backend/PocketcBackendApplication.java
+++ pocketc_backend/src/main/java/com/ssafy/pocketc_backend/PocketcBackendApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PocketcBackendApplication {
-
 	public static void main(String[] args) {
-		System.out.println("HelloWorld!");
 		SpringApplication.run(PocketcBackendApplication.class, args);
 	}
-
 }
