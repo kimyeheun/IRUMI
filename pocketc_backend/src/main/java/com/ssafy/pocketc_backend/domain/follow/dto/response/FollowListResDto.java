309e7af14820955009d653d197d79c8bef8a668b
DESKTOP-HA97GMI\SSAFY <sungmin91529@gmail.com>
2025-09-17 18:07:59 +0900

    feat: follow

---
+++ pocketc_backend/src/main/java/com/ssafy/pocketc_backend/domain/follow/dto/response/FollowListResDto.java
@@ -0,0 +1,11 @@
+package com.ssafy.pocketc_backend.domain.follow.dto.response;
+
+import com.ssafy.pocketc_backend.domain.follow.entity.Follow;
+
+import java.util.List;
+
+public record FollowListResDto (
+        List<Follow> follows
+) {
+
+}
