8f810ab16dfa64b88ee27da47d48a06f5807ea2e
김나연 <0324skdus@naver.com>
2025-09-26 12:31:01 +0900

    fix: 팔로우 이미지,이름 조회되게 수정

--- pocketc_backend/src/main/java/com/ssafy/pocketc_backend/domain/follow/dto/response/FollowResDto.java
+++ pocketc_backend/src/main/java/com/ssafy/pocketc_backend/domain/follow/dto/response/FollowResDto.java
@@ -6,11 +6,15 @@ import java.time.LocalDateTime;
 
 public record FollowResDto(
         Integer followeeId,
+        String followeeName,
+        String followeeProfile,
         LocalDateTime followedAt
 ) {
     public static FollowResDto from(Follow follow) {
         return new FollowResDto(
                 follow.getFollowee().getUserId(),
+                follow.getFollowee().getName(),
+                follow.getFollowee().getProfileImageUrl(),
                 follow.getCreatedAt()
         );
     }
